ee00ac944082b97f91d72b15f065eb77b8c652b3
21020116NguyenQuangAnh <21020116@vnu.edu.vn>
2024-10-02 01:52:36 +0700

    Methods and interfaces

---
+++ QuangAnh/Methods and interfaces/image.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"golang.org/x/tour/pic"
+)
+
+type Image struct{
+	w, h int
+}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBA64Model
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 100, 100)
+}
+
+func (i Image) At(x, y int) color.Color {
+	return color.RGBA{uint8(x), uint8(y), 255, 255}
+}
+
+func main() {
+	m := Image{}
+	pic.ShowImage(m)
+}
